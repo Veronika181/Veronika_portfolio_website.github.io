29e584cce55080fef2bda1d45ed83b0d638f829f
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-06-03 10:18:46 +0200

    commit

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// Získání modálního okna
+var modal = document.getElementById("contactModal");
+
+// Získání tlačítka, které otevře modální okno
+var btn = document.getElementById("contactBtn");
+
+// Získání elementu <span>, který zavře modální okno
+var span = document.getElementsByClassName("close")[0];
+
+// Otevření modálního okna po kliknutí na tlačítko
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Zavření modálního okna po kliknutí na <span> (x)
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Zavření modálního okna po kliknutí mimo něj
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
