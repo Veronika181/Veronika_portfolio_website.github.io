9e91f602c25c64b3640ec00dcb3c3fafd5693762
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-06-10 19:58:07 +0200

    commit

--- javascript.js
+++ javascript.js
@@ -89,3 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
         navbar.classList.remove('active');
     });
 });
+document.getElementById("menu-icon").addEventListener("click", function() {
+    var navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("show");
+});
