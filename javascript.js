3cc72a08311e0fa84fe106e2c276ef2925ff8b9b
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-06-13 16:02:20 +0200

    commit

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navbar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", function() {
+    var sidebar = document.querySelector('.sidebar');
+    var menuButton = document.querySelector('.menu-button');
+    var closeSidebarButton = document.querySelector('.close-sidebar');
 
-    menuIcon.addEventListener("click", function () {
-        navbar.classList.toggle("show");
+    menuButton.addEventListener('click', function() {
+        sidebar.classList.toggle('show');
+    });
+
+    closeSidebarButton.addEventListener('click', function() {
+        sidebar.classList.remove('show');
+    });
+
+    document.querySelectorAll('.sidebar-list a').forEach(function(link) {
+        link.addEventListener('click', function() {
+            sidebar.classList.remove('show');
+        });
     });
 });
+
+
 var typed = new Typed(".animated-text", {
     strings:["Veronika Obrtelova"],
     typeSpeed: 20,
@@ -15,5 +28,38 @@ var typed = new Typed(".animated-text", {
 });
 
 
+const nodemailer = require('nodemailer');
+
+exports.handler = async (event, context) => {
+    const { firstname, lastname, subject } = JSON.parse(event.body);
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'your-email@gmail.com', // nahraďte vlastní emailovou adresou
+            pass: 'your-email-password'   // nahraďte vlastním heslem
+        }
+    });
+
+    let mailOptions = {
+        from: 'your-email@gmail.com',
+        to: 'recipient-email@example.com', // nahraďte cílovou emailovou adresou
+        subject: `New contact from ${firstname} ${lastname}`,
+        text: subject
+    };
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Email sent: ' + info.response })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: error.toString() })
+        };
+    }
+};
 
 
