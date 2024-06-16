9430d2724c88dec96993df9e71d6b139156def76
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-06-16 10:46:49 +0200

    commit

--- javascript.js
+++ javascript.js
@@ -1,16 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Toggle sidebar functionality
-    var sidebar = document.querySelector('.sidebar');
-    var menuButton = document.querySelector('.menu-button');
-    var closeSidebarButton = document.querySelector('.close-sidebar');
-
-    menuButton.addEventListener('click', function() {
-        sidebar.classList.toggle('show');
-    });
-
-    closeSidebarButton.addEventListener('click', function() {
-        sidebar.classList.remove('show');
-    });
 
     document.querySelectorAll('.sidebar-list a').forEach(function(link) {
         link.addEventListener('click', function(e) {
@@ -26,16 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // Initialize Typed.js if it's available
-    if (typeof Typed !== 'undefined') {
-        var typed = new Typed(".animated-text", {
-            strings: ["Veronika Obrtelova"],
-            typeSpeed: 20,
-            backSpeed: 5,
-            startDelay: 500,
-            loop: true
-        });
-    }
+   
 
     // Smooth scrolling for all anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
